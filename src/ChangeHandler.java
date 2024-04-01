67a0718549214b9b0b05c9c47b1cf7259058efd6
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-04-01 20:42:31 +0300

    added base of handler class

---
+++ src/ChangeHandler.java
@@ -0,0 +1,11 @@
+public class ChangeHandler {
+    private int type;
+    /*
+     * 0 - change one cell
+     * 1 - deleted cells
+     * 2 - added cells
+     */
+    private String [] changedCells;
+    private int offt;
+    private int len; //for one cell - 1
+}
