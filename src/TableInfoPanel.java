46ebb32bac6e52acaedc1b570745bf87993b2e10
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-05-09 14:24:22 +0300

    changed TableInfoPanel

--- src/TableInfoPanel.java
+++ src/TableInfoPanel.java
@@ -4,28 +4,50 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class TableInfoPanel extends JPanel {
-    private JLabel sizeLabel;
+    private JLabel sizeLabel, lenLabel;
     private JLabel focusLabel;
-    private JLabel selectionStartLabel;
+    private JLabel selectionStartLabel, mainLabel;
     private JButton changeButton;
 
     public TableInfoPanel(int sizeX, int sizeY, int focus, int selectionStart) {
-        setLayout(new GridLayout(3, 2)); // 3 строки, 2 столбца
+        setLayout(null);
+
+        Font font20 = new Font("Arial", Font.PLAIN, 20);
+        Font font15 = new Font("Arial", Font.PLAIN, 15);
+
         setBorder(new RoundedBorder(10));
-        // Размеры
-        sizeLabel = new JLabel("Размер: " + sizeX + " x " + sizeY + " ячеек");
+
+        mainLabel = new JLabel(" Фокус  -  выделение");
+        mainLabel.setFont(font20);
+        mainLabel.setBounds(40, 10, 200, 20);
+        add(mainLabel);
+
+        sizeLabel = new JLabel("Размер: " + sizeX + " x " + sizeY + " яч.");
+        sizeLabel.setFont(font15);
+        sizeLabel.setBounds(10, 50, 180, 20);
         add(sizeLabel);
 
         changeButton = new JButton("Изменить");
+        changeButton.setFont(font15);
+        changeButton.setBounds(165, 50, 105, 20);
         add(changeButton);
 
         // Текущий фокус
-        focusLabel = new JLabel("Текущий фокус: " + focus);
+        focusLabel = new JLabel("Полож. курсора:      " + focus);
+        focusLabel.setFont(font15);
+        focusLabel.setBounds(11, 80, 200, 20);
         add(focusLabel);
 
         // Начало выделения
         selectionStartLabel = new JLabel("Начало выделения: " + selectionStart);
+        selectionStartLabel.setFont(font15);
+        selectionStartLabel.setBounds(10, 110, 200, 20);
         add(selectionStartLabel);
+
+        lenLabel = new JLabel("Длина выделения:   " + selectionStart);
+        lenLabel.setFont(font15);
+        lenLabel.setBounds(10, 140, 200, 20);
+        add(lenLabel);
 
         // Добавление обработчика событий для кнопки "Изменить"
         changeButton.addActionListener(new ActionListener() {
