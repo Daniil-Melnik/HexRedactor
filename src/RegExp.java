2b4deae08bf3b273229e36b30510cc5ffd1ad795
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-07-17 15:05:28 +0300

    added initial of javadoc to RegExp.java

--- src/RegExp.java
+++ src/RegExp.java
@@ -1,66 +1,81 @@
-/*
-* Класс-утилита
-* Назначение: работа по валидации на регулярных выражениях
-* */
-
 import java.util.ArrayList;
+
+/**
+ * <p>
+ * Class for validating through regular expressions:
+ * </p>
+ * <list>
+ * <li>byte arrays</li>
+ * <li>data types</li>
+ * </list>
+ * 
+ * @see ArrayList
+ * @version 1.0
+ * @author DMelnik
+ */
 
 public class RegExp {
 
     ////////////////////////////////////////////////////////
     //////////////// Валидация одного байта ////////////////
     ////////////////////////////////////////////////////////
-    public boolean isValid(String str){
+    /**
+     * Сhecking the validity of one byte
+     * 
+     * @param str byte value in string interpretation to be checked
+     */
+
+    public boolean isValid(String str) {
         return (str.matches("^[0-9A-Fa-f]{2}$"));
     }
 
     ///////////////////////////////////////////////////////
     ////////////////// Проверка на маску //////////////////
     ///////////////////////////////////////////////////////
-    public boolean isMask(String str){
+    public boolean isMask(String str) {
         return (str.matches("^[10*]{1,64}$"));
     }
 
     ///////////////////////////////////////////////////////
     /////// Проверка на беззнаковое целое значение ////////
     ///////////////////////////////////////////////////////
-    public boolean isValue(String str){
+    public boolean isValue(String str) {
         return (str.matches("^[0-9]{1,10}$"));
     }
 
     ////////////////////////////////////////////////////////
     /////////////// Валидация массива байтов ///////////////
     ////////////////////////////////////////////////////////
-    public ArrayList<Integer> isValidArr(String [] arr, int offt){
+    public ArrayList<Integer> isValidArr(String[] arr, int offt) {
         ArrayList<Integer> res = new ArrayList<Integer>();
-        for (int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             if (!isValid(arr[i])) {
                 res.add(offt + i);
             }
         }
-        return  res;
+        return res;
     }
 
     /////////////////////////////////////////////////////////
     /////////// Условная валидация массива байтов ///////////
     /////////////////////////////////////////////////////////
-    public boolean isValidArrBool(String [] arr){
+    public boolean isValidArrBool(String[] arr) {
         boolean res = true;
         int i = 0;
-        while (res && (i < arr.length)){
+        while (res && (i < arr.length)) {
             res = isValid(arr[i]);
             i++;
         }
-        return  res;
+        return res;
     }
 
     ////////////////////////////////////////////////////////
     ////////////// Получение координат ошибок //////////////
     ////////////////////////////////////////////////////////
-    public int [][] getErrorCells(int rowLen, int offt, ArrayList<Integer> errorOfft){
-        int [][] res = new int[errorOfft.size()][2];
+    public int[][] getErrorCells(int rowLen, int offt, ArrayList<Integer> errorOfft) {
+        int[][] res = new int[errorOfft.size()][2];
         int k;
-        for (Integer i : errorOfft){
+        for (Integer i : errorOfft) {
             k = errorOfft.indexOf(i);
             res[k][0] = (i - offt) / rowLen;
             res[k][1] = (i - offt) % rowLen + 1;
