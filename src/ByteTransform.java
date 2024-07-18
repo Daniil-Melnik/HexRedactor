4b871fcbf4c548f97b4a8a3325a6cf9a2cafdb50
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-07-18 15:49:20 +0300

    added annotations to ByteTransform.java

--- src/ByteTransform.java
+++ src/ByteTransform.java
@@ -1,25 +1,37 @@
 import java.math.BigInteger;
 
-/*
-* Класс с утилитами
-* Назначение:
-* 1) Преобразование байт или блоков байт в А2
-* 2) Выполнение операций поиска
-* */
+/**
+ * Utility class for byte transformation and operations.
+ * <p>
+ * This class provides methods to:
+ * <ul>
+ * <li>Convert bytes or blocks of bytes to binary, signed, unsigned, float, and
+ * double values.</li>
+ * <li>Perform search operations on byte arrays.</li>
+ * </ul>
+ * 
+ * @autor DMelnik
+ * @version 1.0
+ */
 
 public class ByteTransform {
-    
-    /////////////////////////////////////////////////////////////// offt - сдвиг в таблице
-    /////////////// Вернуть двоичную строку из байт ///////////////
-    ///////////////////////////////////////////////////////////////
-    public String getBinaryStr(String [] data, int offt, int len){
-        int [] inIntArr = new int[len];
-        for (int i = 0; i < len; i++){
+
+    /**
+     * Converts bytes to a binary string.
+     * 
+     * @param data the input byte array in hexadecimal format
+     * @param offt the offset in the array
+     * @param len  the length of bytes to convert
+     * @return a binary string representation of the specified bytes
+     */
+    public String getBinaryStr(String[] data, int offt, int len) {
+        int[] inIntArr = new int[len];
+        for (int i = 0; i < len; i++) {
             inIntArr[i] = Integer.parseInt(data[offt + i], 16);
         }
 
-        String [] binaryArr = new String [len];
-        for (int j = 0; j < len; j++){
+        String[] binaryArr = new String[len];
+        for (int j = 0; j < len; j++) {
             binaryArr[j] = Integer.toBinaryString(inIntArr[j]);
             int p = binaryArr[j].length();
             int numberOfZeros = 8 - p;
@@ -32,17 +44,23 @@ public class ByteTransform {
                 binaryArr[j] = zeros + binaryArr[j];
             }
         }
-        
+
         String concBinStr = "";
-        for (String str : binaryArr) concBinStr += str;
+        for (String str : binaryArr)
+            concBinStr += str;
 
         return concBinStr;
-    }    
-    
-    //////////////////////////////////////////////////////////////// offt - сдвиг в таблице
-    //////////////// Преобразование байт в знаковое ////////////////
-    ////////////////////////////////////////////////////////////////
-    public long getSigned(String [] data, int offt, int len){
+    }
+
+    /**
+     * Converts bytes to a signed long value.
+     * 
+     * @param data the input byte array in hexadecimal format
+     * @param offt the offset in the array
+     * @param len  the length of bytes to convert
+     * @return the signed long value represented by the specified bytes
+     */
+    public long getSigned(String[] data, int offt, int len) {
         String concBinStr = getBinaryStr(data, offt, len);
         String subStr = concBinStr.substring(1);
         long res = Long.parseLong(subStr, 2);
@@ -52,21 +70,31 @@ public class ByteTransform {
         return res;
     }
 
-    ///////////////////////////////////////////////////////////////// offt - сдвиг в таблице
-    /////////////// Преобразование байт в беззнаковое ///////////////
-    /////////////////////////////////////////////////////////////////
-    public BigInteger getUnsigned(String [] data, int offt, int len){
+    /**
+     * Converts bytes to an unsigned BigInteger value.
+     * 
+     * @param data the input byte array in hexadecimal format
+     * @param offt the offset in the array
+     * @param len  the length of bytes to convert
+     * @return the unsigned BigInteger value represented by the specified bytes
+     */
+    public BigInteger getUnsigned(String[] data, int offt, int len) {
         String concBinStr = getBinaryStr(data, offt, len);
 
         BigInteger res = new BigInteger(concBinStr, 2);
-   
+
         return res;
     }
 
-    /////////////////////////////////////////////////////////////// offt - сдвиг в таблице
-    //////// Преобразование байт в вещественное 1 точночти ////////
-    ///////////////////////////////////////////////////////////////
-    public float getFloat(String [] data, int offt, int len){
+    /**
+     * Converts bytes to a float value.
+     * 
+     * @param data the input byte array in hexadecimal format
+     * @param offt the offset in the array
+     * @param len  the length of bytes to convert
+     * @return the float value represented by the specified bytes
+     */
+    public float getFloat(String[] data, int offt, int len) {
         String concBinStr = getBinaryStr(data, offt, len);
 
         String subStr = concBinStr.substring(1);
@@ -78,43 +106,53 @@ public class ByteTransform {
         return res;
     }
 
-    /////////////////////////////////////////////////////////////// offt - сдвиг в таблице
-    //////// Преобразование байт в вещественное 2 точночти ////////
-    ///////////////////////////////////////////////////////////////
-    public double getDouble(String [] data, int offt, int len){
+    /**
+     * Converts bytes to a double value.
+     * 
+     * @param data the input byte array in hexadecimal format
+     * @param offt the offset in the array
+     * @param len  the length of bytes to convert
+     * @return the double value represented by the specified bytes
+     */
+    public double getDouble(String[] data, int offt, int len) {
         String concBinStr = getBinaryStr(data, offt, len);
         String subStr = concBinStr.substring(1);
         long longValue = Long.parseLong(subStr, 2);
         double res = Double.longBitsToDouble(longValue);
-        
+
         res *= concBinStr.charAt(0) == '1' ? -1 : 1;
 
         return res;
     }
 
-    ////////////////////////////////////////////////////////////////
-    //////////////////// Поиск блоков по целому ////////////////////
-    ////////////////////////////////////////////////////////////////
-    public int [] getByteOffsetsValue(String [] data, int len, BigInteger  num){
+    /**
+     * Searches for blocks with a specific integer value.
+     * 
+     * @param data the input byte array in hexadecimal format
+     * @param len  the length of the blocks to search
+     * @param num  the target integer value as BigInteger
+     * @return an array of offsets where the target value is found
+     */
+    public int[] getByteOffsetsValue(String[] data, int len, BigInteger num) {
         int cnt = 0;
         String cntStr = "";
         String targetStr = num.toString(2);
         int tmp = len * 8 - targetStr.length();
-        for (int i = 0; i < tmp; i++){
+        for (int i = 0; i < tmp; i++) {
             targetStr = "0" + targetStr;
         }
-        for (int i = 0; i < data.length - 7; i++){
+        for (int i = 0; i < data.length - 7; i++) {
             cntStr = getBinaryStr(data, i, len);
-            if (cntStr.equals(targetStr)){
+            if (cntStr.equals(targetStr)) {
                 cnt++;
             }
         }
 
-        int [] res = new int[cnt];
+        int[] res = new int[cnt];
         int tCnt = 0;
-        for (int j = 0; j < data.length - 7; j++){
+        for (int j = 0; j < data.length - 7; j++) {
             cntStr = getBinaryStr(data, j, len);
-            if (cntStr.equals(targetStr)){
+            if (cntStr.equals(targetStr)) {
                 res[tCnt] = j;
                 tCnt++;
             }
@@ -123,54 +161,67 @@ public class ByteTransform {
         return res;
     }
 
-    ////////////////////////////////////////////////////////////////
-    /////////// Проверка валидности байта в А2 маске ///////////////
-    ////////////////////////////////////////////////////////////////
-    public boolean isValidForMask(String dataEl, String mask){
+    /**
+     * Checks if a byte is valid according to a mask.
+     * 
+     * @param dataEl the byte to check
+     * @param mask   the mask to use for validation
+     * @return true if the byte is valid, false otherwise
+     */
+    public boolean isValidForMask(String dataEl, String mask) {
         int cnt = 0;
 
-        for (int i = 0; i < mask.length(); i++){
-            if ((dataEl.charAt(cnt) == mask.charAt(cnt)) || (mask.charAt(cnt) == '*')){
+        for (int i = 0; i < mask.length(); i++) {
+            if ((dataEl.charAt(cnt) == mask.charAt(cnt)) || (mask.charAt(cnt) == '*')) {
                 cnt++;
             }
         }
         return mask.length() == cnt;
     }
 
-    ////////////////////////////////////////////////////////////////
-    /////////////// Подгонка маски под размер блока ////////////////
-    ////////////////////////////////////////////////////////////////
-    public String updMask (String oldMask, int currLen){
+    /**
+     * Adjusts the mask to fit the size of the block.
+     * 
+     * @param oldMask the original mask
+     * @param currLen the current length of the block
+     * @return the adjusted mask
+     */
+    public String updMask(String oldMask, int currLen) {
         String res = oldMask;
         int oldLen = oldMask.length();
-        if (oldLen < currLen){
+        if (oldLen < currLen) {
             int disLen = currLen - oldLen;
-            for (int i = 0; i < disLen; i++){
+            for (int i = 0; i < disLen; i++) {
                 res += "*";
             }
-        }
-        else if (oldLen > currLen){
+        } else if (oldLen > currLen) {
             res = oldMask.substring(0, currLen);
         }
-        return  res;
+        return res;
     }
 
-    ////////////////////////////////////////////////////////////////
-    //////////// Получить набор сдвигов валидных блоков ////////////
-    ////////////////////////////////////////////////////////////////
-    public int [] getBytesOffsetMask(String [] data, int len, String mask){
+    /**
+     * Gets the offsets of valid blocks according to a mask.
+     * 
+     * @param data the input byte array in hexadecimal format
+     * @param len  the length of the blocks to check
+     * @param mask the mask to use for validation
+     * @return an array of offsets where the valid blocks are found
+     */
+    public int[] getBytesOffsetMask(String[] data, int len, String mask) {
         int numEl = 0;
         int maskByteLen = 8;
         String newMask = updMask(mask, len * maskByteLen);
-        for (int i = 0; i < data.length - maskByteLen + 1; i++){
+        for (int i = 0; i < data.length - maskByteLen + 1; i++) {
             String tmpStr = getBinaryStr(data, i, len);
-            if (isValidForMask(tmpStr, newMask)) numEl++;
+            if (isValidForMask(tmpStr, newMask))
+                numEl++;
         }
 
-        int [] res = new int [numEl];
+        int[] res = new int[numEl];
         int k = 0;
 
-        for (int i = 0; i < data.length - maskByteLen + 1; i++){
+        for (int i = 0; i < data.length - maskByteLen + 1; i++) {
             String tmpStr = getBinaryStr(data, i, len);
             if (isValidForMask(tmpStr, newMask)) {
                 res[k] = i;
