c89527e2668e202d47db372530c2691f3ae0846e
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-07-18 15:53:28 +0300

    added annotations to ChangeSizeDialog.java

--- src/ChangeSizeDialog.java
+++ src/ChangeSizeDialog.java
@@ -1,18 +1,28 @@
-// DualInputDialogExample.java
 import javax.swing.*;
 import java.awt.*;
 
-/*
-* Класс-панель
-* Служит для описания диалогового окна
-* при изменении размеров таблицы
-* */
+/**
+ * Class representing a panel for the dialog window used to change table
+ * dimensions.
+ * <p>
+ * This panel provides input fields for the user to specify the new height and
+ * width
+ * of the table.
+ * </p>
+ * 
+ * @autor DMelnik
+ * @version 1.0
+ */
 
 public class ChangeSizeDialog extends JPanel {
     private JTextField textField1;
     private JTextField textField2;
 
-    public ChangeSizeDialog() {  
+    /**
+     * Constructs a ChangeSizeDialog with input fields for height and width.
+     */
+
+    public ChangeSizeDialog() {
         int rows = 2;
         int cols = 2;
 
@@ -32,9 +42,21 @@ public class ChangeSizeDialog extends JPanel {
         add(textField2);
     }
 
+    /**
+     * Gets the height value from the input field.
+     * 
+     * @return the height value as a String
+     */
+
     public String getHeightValue() {
         return textField1.getText();
     }
+
+    /**
+     * Gets the width value from the input field.
+     * 
+     * @return the width value as a String
+     */
 
     public String getWidthValue() {
         return textField2.getText();
