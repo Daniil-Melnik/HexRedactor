d6e3832e6067cd927687159d9b5031548902cb3b
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-07-18 15:51:51 +0300

    added annotations to ChangeHandler.java

--- src/ChangeHandler.java
+++ src/ChangeHandler.java
@@ -1,39 +1,83 @@
-/*
-* Класс для описания операции над данными
-* */
+/**
+ * Class representing an operation on data.
+ * <p>
+ * This class encapsulates the details of different types of data operations
+ * such as modification, deletion, and insertion.
+ * </p>
+ * <p>
+ * Operation types:
+ * <ul>
+ * <li>0 - Modify a single cell</li>
+ * <li>1 - Delete with zeroing</li>
+ * <li>2 - Delete with shifting</li>
+ * <li>3 - Insert with replacement</li>
+ * <li>4 - Insert with shifting</li>
+ * <li>7 - Insert zeros</li>
+ * </ul>
+ * </p>
+ * 
+ * @autor DMelnik
+ * @version 1.0
+ */
 public class ChangeHandler {
     private int type;
-    /*
-     * 0 - изменение одной ячейки
-     * 1 - удаление с обнулением
-     * 2 - удаление со сдвигом
-     * 3 - вставка с заменой
-     * 4 - вставка со сдвигом
-     * 7 - вставка нулей
-     */
-    private String [] changedCells;
+    private String[] changedCells;
     private int offt;
     private int len;
 
-    public ChangeHandler(int type, int offt, int len, String [] data){
+    /**
+     * Constructs a ChangeHandler with specified type, offset, length, and data.
+     * 
+     * @param type the type of operation
+     * @param offt the offset for the operation
+     * @param len  the length affected by the operation
+     * @param data the data involved in the operation
+     */
+
+    public ChangeHandler(int type, int offt, int len, String[] data) {
         this.type = type;
         this.offt = offt;
         this.len = len;
         this.changedCells = data;
     }
 
-    public int getOfft(){
+    /**
+     * Gets the offset for the operation.
+     * 
+     * @return the offset
+     */
+
+    public int getOfft() {
         return this.offt;
     }
 
-    public int getLen(){
+    /**
+     * Gets the length affected by the operation.
+     * 
+     * @return the length
+     */
+
+    public int getLen() {
         return this.len;
     }
 
-    public int getType(){
+    /**
+     * Gets the type of operation.
+     * 
+     * @return the type of operation
+     */
+
+    public int getType() {
         return this.type;
     }
-    public String [] getData(){
+
+    /**
+     * Gets the data involved in the operation.
+     * 
+     * @return the data as a String array
+     */
+
+    public String[] getData() {
         return this.changedCells;
     }
 }
