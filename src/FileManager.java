e214e22ad94859bb5d14e46b86020996bcc66798
Daniil-Melnik <daniil.melnik.03@bk.ru>
2024-07-18 16:02:19 +0300

    added annotations to FileManager.java

--- src/FileManager.java
+++ src/FileManager.java
@@ -4,15 +4,23 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
-/*
-* Класс с утилитами
-* Назначение: работа с файлами
-* */
+/**
+ * Utility class for file operations.
+ * Provides methods to copy, move, and clear files.
+ * 
+ * @autor DMelnik
+ * @version 1.0
+ */
 
 public class FileManager {
-    ///////////////////////////////////////////////////////////////
-    ////////////////////// Переставить файлы //////////////////////
-    ///////////////////////////////////////////////////////////////
+    /**
+     * Moves or copies a file from the old path to the new path.
+     *
+     * @param fNameOld the path of the source file
+     * @param fNameNew the path of the destination file
+     * @param isCopy   if true, the file is copied; if false, the file is moved
+     * @throws IOException if an I/O error occurs
+     */
     public void setFile(String fNameOld, String fNameNew, boolean isCopy) throws IOException {
         if (!isCopy)
             clearFile(fNameNew);
@@ -21,9 +29,13 @@ public class FileManager {
         Files.delete(pathOld);
     }
 
-    ///////////////////////////////////////////////////////////////
-    /////////////////////// Копировать файл ///////////////////////
-    ///////////////////////////////////////////////////////////////
+    /**
+     * Copies a file using streams.
+     *
+     * @param fNameOld the path of the source file
+     * @param fNameNew the path of the destination file
+     * @throws IOException if an I/O error occurs
+     */
     private static void copyFileUsingStream(String fNameOld, String fNameNew) throws IOException {
         File source = new File(fNameOld);
         File dest = new File(fNameNew);
@@ -44,13 +56,12 @@ public class FileManager {
         }
     }
 
-    ///////////////////////////////////////////////////////////////
-    //////////////////////// Очистить файл ////////////////////////
-    ///////////////////////////////////////////////////////////////
-    // public void clearFile(String fName) throws IOException {
-    // Path path = Paths.get(fName);
-    // Files.writeString(path, new byte[0]);
-    // }
+    /**
+     * Clears the content of a file.
+     *
+     * @param fName the path of the file to clear
+     * @throws IOException if an I/O error occurs
+     */
 
     public void clearFile(String fName) throws IOException {
         Path path = Paths.get(fName);
